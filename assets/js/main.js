621c793d93092a0f44ad24181aa8da25447f9bd0
Saswata Pal <saswata.career@gmail.com>
2021-07-10 21:19:54 +0530

    base url defined

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
+const baseUrl =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "./../../"
+    : "https://github.com/saswatawork/sweet-home-land";
+
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("./../../serviceWorker.js")
+      .register(`${baseUrl}/serviceWorker.js`)
       .then(() => console.log("Service worker Registered"))
       .catch((err) => console.warn(`Service worker error: ${err}`));
   });
